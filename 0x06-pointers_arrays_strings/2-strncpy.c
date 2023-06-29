3069f0e9940488af896a6186f2b8c2066b97b1ca
Tiedes98 <dtierwiir@gmail.com>
2023-06-29 08:01:56 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,13 +2,13 @@
 
 
 /**
- * strncpy - copy a string
+ * _strncpy - copy a string
  * @dest: input value
  * @src: input value
  * @n: input value
  * Return: dest
  */
-char *_strncpy(char *dest, char, *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
